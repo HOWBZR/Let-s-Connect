edeba7629195156977260bd23ba7bcae082655e6
David Coons <dlc9005@gmail.com>
2020-07-15 18:58:48 -0700

    added links and functionality for logging in and out

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -24,7 +24,7 @@ router.get("/", auth, async (req, res) => {
 //@desc     Authenticate User & Get token
 //@access   Public
 
-router.put(
+router.post(
   "/",
   [
     check("email", "Please include a valid email").isEmail(),
